665370976587eadf9dff5bf031a5fad74f80905c
phatn <pngo5@student.gsu.edu>
2021-01-05 23:23:54 -0500

    New names

--- web_scraping_v2.py
+++ web_scraping_v2.py
@@ -7,7 +7,7 @@ https://stackoverflow.com/questions/36026676/python-selenium-timeout-exception-c
 https://wonderproxy.com/blog/a-step-by-step-guide-to-setting-up-a-proxy-in-selenium/   
 https://towardsdatascience.com/selenium-tutorial-scraping-glassdoor-com-in-10-minutes-3d0915c6d905 
 https://www.simplilearn.com/tutorials/selenium-tutorial/selenium-interview-questions-and-answers
-https://sqa.stackexchange.com/questions/33778/chromedriver-in-headless-mode-doesnt-work-correctly-because-of-windows-user-pol
+
 
 Free = proxy 
 
@@ -30,10 +30,11 @@ import time
 import pandas as pd
 
 def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):   
+    #Proxy and verbose are TRUE OR FALSE VALUES ONLY
     
-    #https://github.com/SeleniumHQ/selenium/issues/4685
-    options = webdriver.ChromeOptions()
-    options.headless = True #Make sure the browser doesnt show up, and its run in the background.....
+    
+    options = webdriver.ChromeOptions() # Picking the brower type
+    options.headless = False #Make sure the browser doesnt show up, and its run in the background.....
     
     '''Gathers jobs as a dataframe, scraped from Glassdoor'''
     if proxy:     
@@ -42,44 +43,46 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
              options.add_argument('--proxy-server=%s' % PROXY)
      
             
-    driver = webdriver.Chrome(executable_path=path, options=options)
-    #driver.set_window_size(1700, 1700) 
-    actions = ActionChains(driver)
-    #driver.get("https://www.iplocation.net/find-ip-address")
-    
-    # url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword="+keyword+"&sc.keyword="+keyword+"&locT=S&locId=3426&jobType="
-    url ='https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=&sc.keyword=&locT=&locId=&jobType='  
-    driver.set_window_size(1700, 1700) #Window size                 
-    driver.get(url)
+    driver = webdriver.Chrome(executable_path=path, options=options) #Setting driver
+    driver.set_window_size(1700, 1700) #Window size
+    actions = ActionChains(driver) # Action movement
+    url ='https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=&sc.keyword=&locT=&locId=&jobType='                   
+    driver.get(url) #Getting the URL
     jobs = []
     
-
-    #Uses first variable in function to input job title
-    search = driver.find_element_by_id("LocationSearch").clear()
-    search = driver.find_element_by_id("KeywordSearch")
-    search.send_keys(keyword)
-    
-    #Uses second variable in function to input Location.  Use 'City,State OR Region'
-    search = driver.find_element_by_id("LocationSearch").clear()
-    search = driver.find_element_by_id("LocationSearch")
-    search.send_keys(location)
-    search.send_keys(Keys.RETURN)
+    try:
+        #Thanks stackoverflow       
+        #Uses first variable in function to input job title
+        search = driver.find_element_by_id("LocationSearch").clear()
+        search = driver.find_element_by_id("KeywordSearch")
+        search.send_keys(keyword)
+        
+        #Uses second variable in function to input Location.  Use 'City,State OR Region'
+        search = driver.find_element_by_id("LocationSearch").clear()
+        search = driver.find_element_by_id("LocationSearch")
+        search.send_keys(location)
+        search.send_keys(Keys.RETURN)
+    except:
+          print("Search Error")
+           
     
     #Test for the "Sign Up" prompt and get rid of it.
     time.sleep(slp_time)
     
     #Using this so the program can ignore the exceptions
     ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,TimeoutException) 
+    # Making sure that the filter only go off once
+    count = 0 
+    
 
 
     while len(jobs) < num_jobs:  #If true, should be still looking for new jobs.
-    
-        clickable= False
-        employess =False
-
+       
+        
+       
         #Let the page load. Change this number based on your internet speed.
         #Or, wait until the webpage is loaded, instead of hardcoding it.
-        time.sleep(4)
+        time.sleep(slp_time)
         
 
         #Test for the "Sign Up" prompt and get rid of it.
@@ -87,7 +90,7 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
         
         try:
              selected= WebDriverWait(driver, slp_time).until(
-             EC.element_to_be_clickable((By.CLASS_NAME, "selected")))
+                EC.element_to_be_clickable((By.CLASS_NAME, "selected")))
              selected.click() 
              
              
@@ -103,7 +106,7 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
         except (NoSuchElementException,ElementNotInteractableException):
             pass
         
-        if not clickable: 
+        if count < 2: 
             try:     
                 
                 clickable_filter1=WebDriverWait(driver, slp_time,ignored_exceptions=ignored_exceptions).until(
@@ -118,8 +121,8 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
                 EC.element_to_be_clickable((By.XPATH,'.//div[@class="checkboxBox"]')))
                 driver.execute_script("arguments[0].click();", clickable_filter2)
                 
-                #WebDriverWait(driver, slp_time).until(
-                #EC.presence_of_element_located((By.XPATH,'.//div[@class="checkboxBox"]'))).click()  
+               # WebDriverWait(driver, slp_time).until(
+               # EC.presence_of_element_located((By.XPATH,'.//div[@class="checkboxBox"]'))).click()  
                 
                 clickable_filter3=WebDriverWait(driver, slp_time,ignored_exceptions=ignored_exceptions).until(
                 EC.element_to_be_clickable((By.XPATH,'.//button[@class="applybutton gd-btn gd-btn-link gradient gd-btn-2 gd-btn-sm"]')))
@@ -128,38 +131,39 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
                # WebDriverWait(driver, slp_time).until(
                # EC.presence_of_element_located((By.XPATH, './/button[@class="applybutton gd-btn gd-btn-link gradient gd-btn-2 gd-btn-sm"]'))).click()  
                 
-                clickable = True 
+                count = count + 1
                 
                 
-                print('Salary filter is working')
+                print('Salary filter is working', count)
             except TimeoutException:
                 print('Can not find Salary filter')
                 pass
-            time.sleep(.1)
+            time.sleep(2)
             
-        if not employess: 
+        if count < 2: 
                try:
                    employess_filter1= WebDriverWait(driver, slp_time).until(
-                   EC.element_to_be_clickable((By.XPATH,'.//div[@class="filter more expandable"]')))
+                   EC.presence_of_element_located((By.XPATH,'.//div[@class="filter more expandable"]')))
                    employess_filter1.click()
                    
                    employess_filter2 = WebDriverWait(driver, slp_time).until(
-                   EC.element_to_be_clickable((By.XPATH,'.//div[@class="filter more expandable expanded"]//div[@id="filter_employerSizes"]')))
+                   EC.presence_of_element_located((By.XPATH,'.//div[@class="filter more expandable expanded"]//div[@id="filter_employerSizes"]')))
                    employess_filter2.click()
                    
                    employess_filter3 = WebDriverWait(driver, slp_time).until(
-                   EC.element_to_be_clickable((By.XPATH,'//*[@id="dynamicFiltersContainer"]/div/div[1]/div[2]/div[2]/div[13]/ul/li[6]')))
+                   EC.presence_of_element_located((By.XPATH,'//*[@id="dynamicFiltersContainer"]/div/div[1]/div[2]/div[2]/div[13]/ul/li[6]')))
                    #'.//ul[@class="css-1dv4b0s ew8xong0"]//li[@value="5"]')))
                    employess_filter3.click()
                    
                    time.sleep(1)   
                    
                    employess_filter4 = WebDriverWait(driver, slp_time).until(
-                   EC.element_to_be_clickable((By.XPATH,'.//div[@class="allDropdowns"]//div[@class="filter more expandable expanded applied"]')))
+                   EC.presence_of_element_located((By.XPATH,'.//div[@class="allDropdowns"]//div[@class="filter more expandable expanded applied"]')))
                    employess_filter4.click()
                   
-                   employess = True 
-                   print('Company size filter working')
+                   count = count + 1 
+                   
+                   print('Company size filter working',count)
                except (NoSuchElementException,TimeoutException,StaleElementReferenceException,ElementNotInteractableException):               
                    print('Company filter failed')     
 
@@ -182,6 +186,7 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
             #You might 
             time.sleep(1)
             collected_successfully = False
+            Rating_collected_successfully =False
             
             while not collected_successfully:
               
@@ -205,30 +210,71 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
                 try: 
                     job_description = WebDriverWait(driver, slp_time).until(
                     EC.visibility_of_element_located((By.XPATH,'.//div[@class="jobDescriptionContent desc"]'))).text              
-                    collected_successfully = True
+                    
                                        
                 except (NoSuchElementException,TimeoutException,ElementNotInteractableException,StaleElementReferenceException):
                     job_description = -1
                     
-
-            try:
-                salary_estimate = driver.find_element_by_xpath('.//span[@class="css-1uyte9r css-hca4ks e1wijj242"]').text
-            except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
-                salary_estimate = -1 #You need to set a "not found value. It's important."
-            
-            try:
-                rating = driver.find_element_by_xpath('.//span[@class="rating"]').text
-            except (NoSuchElementException,ElementNotInteractableException):
-                rating = -1 #You need to set a "not found value. It's important."
+                try:
+                    salary_estimate = driver.find_element_by_xpath('.//span[@class="css-1uyte9r css-hca4ks e1wijj242"]').text
+                    collected_successfully = True
+                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
+                    salary_estimate = -1 #You need to set a "not found value. It's important."    
+                    
+                #--------------------------------------------------------------------------------------------------------------  
+                
+            if verbose:
+             print("Job Title: {}".format(job_title))
+             print("Salary Estimate: {}".format(salary_estimate))
+             print("Job Description: {}".format(job_description[:500]))             
+             print("Company Name: {}".format(company_name))
+             print("Location: {}".format(location))      
+                
+            while not Rating_collected_successfully:  
+                 
+                try:
+                     culture_values = driver.find_element_by_xpath('.//strong[text()="Culture & Values"]//following-sibling::*').text
+                   
+                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):                      
+                       culture_values = -1
+                try:
+                       compensation_benefits = driver.find_element_by_xpath('.//strong[text()="Compensation & Benefits"]//following-sibling::*').text
+                                       
+                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
+                      
+                      compensation_benefits = -1
+                try:
+                    
+                      career_opportunities = driver.find_element_by_xpath('.//strong[text()="Career Opportunities"]//following-sibling::*').text
+                  
+                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
+                      
+                       career_opportunities = -1
+                      
+                try:
+                    work_life_balance = driver.find_element_by_xpath('.//strong[text()="Work/Life Balance"]//following-sibling::*').text
+                 
+                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
+                    work_life_balance = 1
+                        
+                
+                try:
+                    rating = driver.find_element_by_xpath('.//span[@class="rating"]').text
+                    Rating_collected_successfully = True
+                    
+                except (NoSuchElementException,ElementNotInteractableException):
+                    
+                    rating = -1 #You need to set a "not found value. It's important."
+                    
 
             #Printing for debugging
             if verbose:
-                print("Job Title: {}".format(job_title))
-                print("Salary Estimate: {}".format(salary_estimate))
-                print("Job Description: {}".format(job_description[:500]))
-                print("Rating: {}".format(rating))
-                print("Company Name: {}".format(company_name))
-                print("Location: {}".format(location))
+                print("Rating: {}".format(rating))   
+                print("Compensation & Benefits: {}".format(compensation_benefits))
+                print("Culture Values: {}".format(culture_values))
+                print("Career Opportunities {}".format(career_opportunities))
+                print("WorkLife Balance: {}".format(work_life_balance))
+
 
             #Going to the Company tab...
             #clicking on this:
@@ -272,19 +318,16 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
 
               
 
-            except (NoSuchElementException,TimeoutException,ElementClickInterceptedException,ElementNotInteractableException,StaleElementReferenceException):  #Rarely, some job postings do not have the "Company" tab.
-                
-                size = -1
-                founded = -1
-                type_of_ownership = -1
-                industry = -1
-                sector = -1
-                revenue = -1
-                
-
-                
-            if verbose:
-                
+            except (NoSuchElementException,TimeoutException,ElementClickInterceptedException,ElementNotInteractableException,StaleElementReferenceException):  
+                #Rarely, some job postings do not have the "Company" tab.                
+                    size = -1
+                    founded = -1
+                    type_of_ownership = -1
+                    industry = -1
+                    sector = -1
+                    revenue = -1
+                          
+            if verbose: 
                 print("Size: {}".format(size))
                 print("Founded: {}".format(founded))
                 print("Type of Ownership: {}".format(type_of_ownership))
@@ -303,44 +346,11 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
                                        
                 except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):                      
                         ceo = -1
-                try:
-                     culture_values = driver.find_element_by_xpath('.//strong[text()="Culture & Values"]//following-sibling::*').text
-                   
-                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):                      
-                       culture_values = -1
-                try:
-                       compensation_benefits = driver.find_element_by_xpath('.//strong[text()="Compensation & Benefits"]//following-sibling::*').text
-                                       
-                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
-                      
-                      compensation_benefits = -1
-                try:
-                    
-                      career_opportunities = driver.find_element_by_xpath('.//strong[text()="Career Opportunities"]//following-sibling::*').text
-                  
-                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
-                      
-                       career_opportunities = -1
-                      
-                try:
-                    work_life_balance = driver.find_element_by_xpath('.//strong[text()="Work/Life Balance"]//following-sibling::*').text
-                 
-                except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException):
-                       
-                       work_life_balance = -1
-            except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException,TimeoutException):  #Rarely, some job postings do not have the "Company" tab.
-                
-                    compensation_benefits = -1
-                    culture_values = -1
-                    career_opportunities = -1
-                    work_life_balance = -1
+                        
+            except (NoSuchElementException,ElementNotInteractableException,StaleElementReferenceException,TimeoutException):  
+                #Rarely, some job postings do not have the "Company" tab.                
                     ceo = -1
             if verbose:
-                
-                print("Compensation & Benefits: {}".format(compensation_benefits))
-                print("Culture Values: {}".format(culture_values))
-                print("Career Opportunities {}".format(career_opportunities))
-                print("WorkLife Balance: {}".format(work_life_balance))
                 print("Ceo: {}".format(ceo))
 
             jobs.append({"Job Title" : job_title,
@@ -359,6 +369,7 @@ def get_jobs(keyword,location, num_jobs, verbose,path,slp_time,proxy):
             "Compensation & Benefits" : compensation_benefits,
             "Career Opportunities" : career_opportunities,
             "Work Life Balance" : work_life_balance,
+            "Culture Values" : culture_values,
   
             
             })
